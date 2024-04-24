43caf4e1fba1a3440239eabe3699993e475fa3bc
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-04-24 09:06:40 -0600

    Duck button added

--- src/duck/model/InternetDuck.java
+++ src/duck/model/InternetDuck.java
@@ -2,6 +2,9 @@ package duck.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+/**
+ * A record for storing json data about a duck from the internet.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record InternetDuck(String message, String url)
 {
--- src/duck/view/DuckPanel.java
+++ src/duck/view/DuckPanel.java
@@ -1,6 +1,13 @@
 package duck.view;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.GridLayout;
 
 import duck.controller.Controller;
 
@@ -8,15 +15,35 @@ public class DuckPanel extends JPanel
 {
 	private Controller app;
 	
+	private SpringLayout layout;
+	private JLabel displayLabel;
+	private JPanel menuPanel;
+	private JButton duckButton;
+	
 	public DuckPanel(Controller app)
 	{
 		super();
 		this.app = app;
+		
+		this.layout = new SpringLayout();
+		this.displayLabel = new JLabel("Debugging Duck");
+		this.menuPanel = new JPanel(new GridLayout(0, 1));
+		this.duckButton = new JButton("Duck");
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
 	}
 	
 	private void setupPanel()
 	{
+		setBackground(Color.CYAN);
+		setLayout(layout);
 		
+		//this.add(displayLabel);
+		this.add(menuPanel);
+		
+		menuPanel.add(duckButton);
 	}
 	
 	private void setupListeners()
@@ -26,6 +53,9 @@ public class DuckPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		layout.putConstraint(SpringLayout.NORTH, menuPanel, 10, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, menuPanel, 10, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, menuPanel, 100, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.SOUTH, menuPanel, 500, SpringLayout.NORTH, this);
 	}
 }
