04a8f1e82401115b66e119bf4f481040adc4c9d8
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-05-06 08:43:31 -0600

    Simple web browser working

--- src/duck/view/DuckBrowserPanel.java
+++ src/duck/view/DuckBrowserPanel.java
@@ -3,11 +3,13 @@ package duck.view;
 import javax.swing.JPanel;
 import javax.swing.event.HyperlinkListener;
 import javax.swing.event.HyperlinkEvent;
+import javax.swing.text.html.HTMLFrameHyperlinkEvent;
+import javax.swing.text.html.HTMLDocument;
 import javax.swing.SpringLayout;
 import javax.swing.JTextField;
 import javax.swing.JEditorPane;
 
-import java.awt.GridBagLayout;
+import java.io.IOException;
 
 import duck.controller.Controller;
 
@@ -41,11 +43,14 @@ public class DuckBrowserPanel extends JPanel
 		
 		this.add(addressBar);
 		this.add(webPane);
+		
+		setWebPage("http://frogfind.com");
 	}
 	
 	private void setupListeners()
 	{
-		
+		addressBar.addActionListener(event -> setWebPage(addressBar.getText()));
+		webPane.addHyperlinkListener(event -> hyperLinkAction(event));
 	}
 	
 	private void setupLayout()
@@ -59,5 +64,39 @@ public class DuckBrowserPanel extends JPanel
 		layout.putConstraint(SpringLayout.WEST, webPane, 0, SpringLayout.WEST, this);
 		layout.putConstraint(SpringLayout.EAST, webPane, 0, SpringLayout.EAST, this);
 		layout.putConstraint(SpringLayout.SOUTH, webPane, 0, SpringLayout.SOUTH, this);
+	}
+	
+	private void setWebPage(String url)
+	{
+		try
+		{
+			webPane.setPage(url);
+		}
+		catch (IOException error)
+		{
+			app.handleError(error);
+		}
+	}
+	
+	private void hyperLinkAction(HyperlinkEvent event)
+	{
+		if (event.getEventType() != HyperlinkEvent.EventType.ACTIVATED)
+		{
+			return;
+		}
+		
+		JEditorPane useless = (JEditorPane)event.getSource();
+		
+		if (event instanceof HTMLFrameHyperlinkEvent)
+		{
+			HTMLDocument doc = (HTMLDocument)webPane.getDocument();
+			doc.processHTMLFrameHyperlinkEvent((HTMLFrameHyperlinkEvent)event);
+		}
+		else
+		{
+			String url = event.getURL().toString();
+			addressBar.setText(url);
+			setWebPage(url);
+		}
 	}
 }
